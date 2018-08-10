0139dcd74a8b4de69430b3d8390f94dc29d3d95e
fanich37 <icima.fan@gmail.com>
2018-08-10 15:40:39 +0300

    rewrite with thunk

--- src/actionCreators/index.js
+++ src/actionCreators/index.js
@@ -1,5 +1,3 @@
-export const openCard = index => ({ type: 'OPEN_CARD', index });
-
 export const closeOpenCards = () => ({ type: 'CLOSE_OPEN_CARDS' });
 
 export const setStage = stage => ({ type: 'SET_STAGE', stage });
@@ -7,4 +5,35 @@ export const setStage = stage => ({ type: 'SET_STAGE', stage });
 export const addFoundCard = foundCards => ({
   type: 'ADD_FOUND_CARD',
   foundCards: [foundCards[0], foundCards[1]]
+});
+
+export const openCard = index => (dispatch, getState) => new Promise(resolve => {
+  const timer = setTimeout(() => {
+    clearTimeout(timer);
+    resolve(index);
+  }, 0);
+}).then(res => {
+  dispatch({ type: 'OPEN_CARD', index: res });
+  const state = getState();
+
+  if (state.openCards.length === 2) {
+    const firstCardIndex = state.openCards[0];
+    const secondCardIndex = state.openCards[1];
+    const firstCard = state.cards[firstCardIndex];
+    const secondCard = state.cards[secondCardIndex];
+
+    return new Promise(resolve => {
+      const timer = setTimeout(() => {
+        clearTimeout(timer);
+        if (firstCard.id === secondCard.id) {
+          resolve(addFoundCard([firstCardIndex, secondCardIndex]));
+        } else {
+          resolve(closeOpenCards());
+        }
+      }, 1000);
+    }).then(action => {
+      dispatch(action);
+    });
+  }
+  return state;
 });
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,58 +7,35 @@ import FinalContainer from '../../containers/FinalContainer';
 import BigCardContainer from '../../containers/BigCardContainer';
 import CounterContainer from '../../containers/CounterContainer';
 import Pics from '../Pics/Pics';
-import { closeOpenCards, addFoundCard } from '../../actionCreators';
 
-class App extends React.Component {
-  componentDidUpdate() {
-    if (this.props.openCards.length === 2) {
-      const firstCardIndex = this.props.openCards[0];
-      const secondCardIndex = this.props.openCards[1];
-      const firstCard = this.props.cards[firstCardIndex];
-      const secondCard = this.props.cards[secondCardIndex];
-      const timer = setTimeout(() => {
-        if (firstCard.id === secondCard.id) {
-          this.props.dispatch(addFoundCard([firstCardIndex, secondCardIndex]));
-        } else {
-          this.props.dispatch(closeOpenCards());
-        }
-        clearTimeout(timer);
-      }, 1000);
-    }
-  }
+const App = ({ sourceData, stage }) => {
+  const gameInnerClassNames = stage === 'running' || stage === 'paused'
+    ? style.game__inner
+    : [style.game__inner, style.game__inner_hidden].join(' ');
 
-  render() {
-    const gameInnerClassNames = this.props.stage === 'running' || this.props.stage === 'paused'
-      ? style.game__inner
-      : [style.game__inner, style.game__inner_hidden].join(' ');
+  const buttonWrapperClassNames = stage === 'start'
+    ? style.game__btnWrapper
+    : [style.game__btnWrapper, style.game__btnWrapper_hidden].join(' ');
 
-    const buttonWrapperClassNames = this.props.stage === 'start'
-      ? style.game__btnWrapper
-      : [style.game__btnWrapper, style.game__btnWrapper_hidden].join(' ');
-
-    return (
-      <div className={style.game}>
-        <div className={buttonWrapperClassNames}>
-          <ButtonContainer modifier="button_big" />
-        </div>
-        <div className={gameInnerClassNames}>
-          <CounterContainer />
-          <CardsContainer />
-          <BigCardContainer />
-          <Pics pics={this.props.sourceData} />
-        </div>
-        <FinalContainer />
+  return (
+    <div className={style.game}>
+      <div className={buttonWrapperClassNames}>
+        <ButtonContainer modifier="button_big" />
       </div>
-    );
-  }
-}
+      <div className={gameInnerClassNames}>
+        <CounterContainer />
+        <CardsContainer />
+        <BigCardContainer />
+        <Pics pics={sourceData} />
+      </div>
+      <FinalContainer />
+    </div>
+  );
+};
 
 App.propTypes = {
-  cards: PropTypes.arrayOf(PropTypes.object).isRequired,
   sourceData: PropTypes.arrayOf(PropTypes.object).isRequired,
-  openCards: PropTypes.arrayOf(PropTypes.number).isRequired,
-  stage: PropTypes.string.isRequired,
-  dispatch: PropTypes.func.isRequired
+  stage: PropTypes.string.isRequired
 };
 
 export default App;

1f1766e0b9fedfaf465b97f6a1d870751adaef9f
fanich37 <icima.fan@gmail.com>
2018-08-17 13:24:25 +0300

    fixed bug with opening card

--- src/actionCreators/index.js
+++ src/actionCreators/index.js
@@ -14,31 +14,33 @@ export const addFoundCard = foundCards => ({
 export const openCard = index => (dispatch, getState) => new Promise(resolve => {
   const timer = setTimeout(() => {
     clearTimeout(timer);
-    resolve(index);
+    const isTwoCardsOpen = getState().openCards.length === 2;
+    resolve(isTwoCardsOpen);
   }, 0);
-}).then(res => {
-  dispatch({ type: OPEN_CARD, index: res });
-  const state = getState();
+}).then(isTwoCardsOpen => {
+  if (!isTwoCardsOpen) {
+    dispatch({ type: OPEN_CARD, index });
 
-  if (state.openCards.length === 2) {
-    const firstCardIndex = state.openCards[0];
-    const secondCardIndex = state.openCards[1];
-    const firstCard = state.cards[firstCardIndex];
-    const secondCard = state.cards[secondCardIndex];
+    const state = getState();
 
-    return new Promise(resolve => {
-      const timer = setTimeout(() => {
-        clearTimeout(timer);
-        if (firstCard.slug === secondCard.slug) {
-          resolve(addFoundCard([firstCardIndex, secondCardIndex]));
-        } else {
-          resolve(closeOpenCards());
-        }
-      }, 1000);
-    }).then(action => {
-      dispatch(action);
-    });
+    if (state.openCards.length === 2) {
+      const firstCardIndex = state.openCards[0];
+      const secondCardIndex = state.openCards[1];
+      const firstCard = state.cards[firstCardIndex];
+      const secondCard = state.cards[secondCardIndex];
+
+      return new Promise(resolve => {
+        const timer = setTimeout(() => {
+          clearTimeout(timer);
+          if (firstCard.slug === secondCard.slug) {
+            resolve(addFoundCard([firstCardIndex, secondCardIndex]));
+          } else {
+            resolve(closeOpenCards());
+          }
+        }, 1000);
+      }).then(action => {
+        dispatch(action);
+      });
+    }
   }
-
-  return state;
 });
